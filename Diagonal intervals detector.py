01b2997598607460d7d4d3323997db1716081bd6
Sylvain Margot <sylvain.margot@mail.mcgill.ca>
2019-05-15 12:56:16 -0400

    V. 1.0: Return all diagonal intervals of a piece, including rests, with entry delay from -8 beats to +8 beats

--- Diagonal intervals detector.py
+++ Diagonal intervals detector.py
@@ -1,37 +1,44 @@
 from music21 import *
 import numpy as np
-from operator import itemgetter
 
 Josquin_secure_corpus=corpus.corpora.LocalCorpus()
 Josquin_secure_corpus.addPath('../Mass duos Josquin - De La Rue/Josquin (secure)/XML')
 Agnus_2=corpus.parse('XML/Josquin Missa Ave maris stella - Agnus II.xml')
 Agnus_2_Parts=instrument.partitionByInstrument(Agnus_2)
 
-Upper_voice=[]
-Lower_voice=[]
+Agnus_2_Upper_voice = {}
+Agnus_2_Lower_voice = {}
+Interval_pattern = []
 
-for Notes_Upper_voice in Agnus_2_Parts.parts['Soprano'].getElementsByClass(note.Note):
-    Upper_voice.append([Notes_Upper_voice, Notes_Upper_voice.offset])
+L_sop=len(Agnus_2_Parts.parts['Soprano'].notesAndRests.stream())
+L_ten=len(Agnus_2_Parts.parts['Tenor'].notesAndRests.stream())
+L=min(L_sop, L_ten)
 
-for Rests_Upper_voice in Agnus_2_Parts.parts['Soprano'].getElementsByClass(note.Rest):
-    Upper_voice.append([Rests_Upper_voice, Rests_Upper_voice.offset])
-
-Upper_voice=sorted(Upper_voice,key=itemgetter(1))
-
-for Notes_Lower_voice in Agnus_2_Parts.parts['Tenor'].getElementsByClass(note.Note):
-    Lower_voice.append([Notes_Lower_voice, Notes_Lower_voice.offset])
-
-for Rests_Lower_voice in Agnus_2_Parts.parts['Tenor'].getElementsByClass(note.Rest):
-    Lower_voice.append([Rests_Lower_voice, Rests_Lower_voice.offset])
-
-Lower_voice=sorted(Lower_voice,key=itemgetter(1))
-
-L=len(Lower_voice)
-
-for i in range (0,L):
+for i in range (0,L_sop):
     try:
-        interval.notesToChromatic(Lower_voice[i][0],Upper_voice[i+1][0])
+        Agnus_2_Parts.parts['Soprano'].notesAndRests.stream()[i].pitch
     except:
-        print("<music21.interval.ChromaticInterval 0>")
+        Agnus_2_Upper_voice[Agnus_2_Parts.parts['Soprano'].notesAndRests.stream()[i].offset]=0
     else:
-        print(interval.notesToChromatic(Lower_voice[i][0],Upper_voice[i+1][0]))
+        Agnus_2_Upper_voice[Agnus_2_Parts.parts['Soprano'].notesAndRests.stream()[i].offset]=Agnus_2_Parts.parts['Soprano'].notesAndRests.stream()[i].pitch
+
+for i in range (0,L_ten):
+    try:
+        Agnus_2_Parts.parts['Tenor'].notesAndRests.stream()[i].pitch
+    except:
+        Agnus_2_Lower_voice[Agnus_2_Parts.parts['Tenor'].notesAndRests.stream()[i].offset]=0
+    else:
+        Agnus_2_Lower_voice[Agnus_2_Parts.parts['Tenor'].notesAndRests.stream()[i].offset]=Agnus_2_Parts.parts['Tenor'].notesAndRests.stream()[i].pitch
+
+for i in range (-8,9):
+    for position in Agnus_2_Lower_voice:
+        if position+i in Agnus_2_Upper_voice:
+            if Agnus_2_Lower_voice[position] == 0:
+                print("D = ",i, "Interval = 0")
+            else:
+                if Agnus_2_Upper_voice[position+i] == 0:
+                    print("D = ",i, "Interval = 0")
+                else:
+                    print("D = ",i, "Interval = ", interval.notesToChromatic(Agnus_2_Lower_voice[position],Agnus_2_Upper_voice[position+i]))
+        else:
+            print("D = ",i, "Interval = 0")
