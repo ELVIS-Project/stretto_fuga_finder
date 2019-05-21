7689c90b69ab0c2900ffe4b838aee6366f68e890
Sylvain Margot <sylvain.margot@mail.mcgill.ca>
2019-05-21 12:27:02 -0400

    V. 1.2 Corrected version

--- Stretto_Fuga_Detector.py
+++ Stretto_Fuga_Detector.py
@@ -5,11 +5,7 @@ import numpy as np
 Josquin_secure_corpus=corpus.corpora.LocalCorpus()
 Josquin_secure_corpus.addPath('../Mass duos Josquin - De La Rue/Josquin (secure)/XML')
 Piece=corpus.parse('XML/Josquin Missa Ave maris stella - Agnus II.xml')
-Piece_Parts=instrument.partitionByInstrument(Piece)
 
-Piece_Upper_voice = OrderedDict()
-Piece_Lower_voice = OrderedDict()
-Interval_pattern = []
 
 def DiagInterval(note1,note2):
     if note1 == 'Rest':
@@ -20,103 +16,122 @@ def DiagInterval(note1,note2):
         return interval.notesToInterval(note1,note2).niceName.split()[1]
 
 
-Values = {
-0: "parallel motion at",
-1: "stretto fuga at the minime",
-2: "stretto fuga at the semi-breve",
-3: "stretto fuga at the doted semi-breve",
-4: "stretto fuga at the breve",
-5: "stretto fuga at the breve and a minime",
-6: "stretto fuga at the dotted breve",
-7: "stretto fuga at the breve and a doted semi-breve",
-8: "stretto fuga at the longa"
-}
+def ImitationDetector(score):
 
-L_upp=len(Piece_Parts.parts[0].notesAndRests.stream())
-L_low=len(Piece_Parts.parts[1].notesAndRests.stream())
-L=min(L_upp, L_low)
+    Values = {
+    0: "parallel motion at",
+    1: "stretto fuga at the crotchet",
+    2: "stretto fuga at the minime",
+    3: "stretto fuga at the doted minime",
+    4: "stretto fuga at the semibreve",
+    5: "stretto fuga at the semibreve and a crotchet",
+    6: "stretto fuga at the dotted semibreve",
+    7: "stretto fuga at the semibreve and a doted minime",
+    8: "stretto fuga at the breve"
+    }
 
-for i in range (0,L_upp):
-    try:
-        Piece_Parts[0].notesAndRests.stream()[i].pitch
-    except:
-        Piece_Upper_voice[Piece_Parts[0].notesAndRests.stream()[i].offset]=["Rest",Piece_Parts[0].notesAndRests.stream()[i].measureNumber]
-    else:
-        Piece_Upper_voice[Piece_Parts[0].notesAndRests.stream()[i].offset]=[Piece_Parts[0].notesAndRests.stream()[i].pitch,Piece_Parts[0].notesAndRests.stream()[i].measureNumber]
+    Parts=instrument.partitionByInstrument(score)
 
-for j in range (0,L_low):
-    try:
-        Piece_Parts.parts[1].notesAndRests.stream()[j].pitch
-    except:
-        Piece_Lower_voice[Piece_Parts[1].notesAndRests.stream()[j].offset]=["Rest",Piece_Parts[1].notesAndRests.stream()[j].measureNumber]
-    else:
-        Piece_Lower_voice[Piece_Parts[1].notesAndRests.stream()[j].offset]=[Piece_Parts[1].notesAndRests.stream()[j].pitch,Piece_Parts[1].notesAndRests.stream()[j].measureNumber]
+    L_upp=len(Parts.parts[0].notesAndRests.stream())
+    L_low=len(Parts.parts[1].notesAndRests.stream())
+    L=min(L_upp, L_low)
 
-for k in range (-8,9):
+    Upper_voice = OrderedDict()
+    Lower_voice = OrderedDict()
 
-    A=[]
+    for i in range (0,L_upp):
+        try:
+            Parts[0].notesAndRests.stream()[i].pitch
+        except:
+            Upper_voice[Parts[0].notesAndRests.stream()[i].offset]=["Rest",Parts[0].notesAndRests.stream()[i].measureNumber]
+        else:
+            Upper_voice[Parts[0].notesAndRests.stream()[i].offset]=[Parts[0].notesAndRests.stream()[i].pitch,Parts[0].notesAndRests.stream()[i].measureNumber]
 
-    if k<0:
-        Imitation="below"
-    elif k == 0:
-        Imitation=""
-    else:
-        Imitation="above"
-    B=0
+    for j in range (0,L_low):
+        try:
+            Parts.parts[1].notesAndRests.stream()[j].pitch
+        except:
+            Lower_voice[Parts[1].notesAndRests.stream()[j].offset]=["Rest",Parts[1].notesAndRests.stream()[j].measureNumber]
+        else:
+            Lower_voice[Parts[1].notesAndRests.stream()[j].offset]=[Parts[1].notesAndRests.stream()[j].pitch,Parts[1].notesAndRests.stream()[j].measureNumber]
 
-    for l in range (0,L-1):
-        if list(Piece_Lower_voice.items())[l][0]+k in Piece_Upper_voice:
-            if list(Piece_Lower_voice.items())[l+1][0]+k in Piece_Upper_voice:
-                if DiagInterval(Piece_Lower_voice[list(Piece_Lower_voice.items())[l][0]][0],Piece_Upper_voice[list(Piece_Lower_voice.items())[l][0]+k][0]) == DiagInterval(Piece_Lower_voice[list(Piece_Lower_voice.items())[l+1][0]][0],Piece_Upper_voice[list(Piece_Lower_voice.items())[l+1][0]+k][0]):
-                    B=B+1
-                    if B==3:
-                        A.append("Measure {}, ".format(Piece_Lower_voice[list(Piece_Lower_voice.items())[l-2][0]][1])+Values[abs(k)]+" a "+DiagInterval(Piece_Lower_voice[list(Piece_Lower_voice.items())[l][0]][0],Piece_Upper_voice[list(Piece_Lower_voice.items())[l][0]+k][0])+" "+Imitation+";")
-                    else:
-                        pass
-                elif DiagInterval(Piece_Lower_voice[list(Piece_Lower_voice.items())[l+1][0]][0],Piece_Upper_voice[list(Piece_Lower_voice.items())[l+1][0]+k][0])=='Rest':
-                    if list(Piece_Lower_voice.items())[l+2][0]+k in Piece_Upper_voice:
-                         if DiagInterval(Piece_Lower_voice[list(Piece_Lower_voice.items())[l][0]][0],Piece_Upper_voice[list(Piece_Lower_voice.items())[l][0]+k][0]) == DiagInterval(Piece_Lower_voice[list(Piece_Lower_voice.items())[l+2][0]][0],Piece_Upper_voice[list(Piece_Lower_voice.items())[l+2][0]+k][0]):
-                             B=B+1
-                         else:
-                             if A!=[]:
-                                 A[-1]=A[-1]+" it ends measure {}".format(Piece_Lower_voice[list(Piece_Lower_voice.items())[l-1][0]][1])+"."
-                             else:
-                                 pass
-                    else:
-                        pass
-                elif DiagInterval(Piece_Lower_voice[list(Piece_Lower_voice.items())[l][0]][0],Piece_Upper_voice[list(Piece_Lower_voice.items())[l][0]+k][0])=='Rest':
-                    if list(Piece_Lower_voice.items())[l-1][0]+k in Piece_Upper_voice:
-                        if DiagInterval(Piece_Lower_voice[list(Piece_Lower_voice.items())[l-1][0]][0],Piece_Upper_voice[list(Piece_Lower_voice.items())[l-1][0]+k][0]) == DiagInterval(Piece_Lower_voice[list(Piece_Lower_voice.items())[l+1][0]][0],Piece_Upper_voice[list(Piece_Lower_voice.items())[l+1][0]+k][0]):
-                         B=B+1
+
+    Interval_pattern = []
+
+    for k in range (-8,9):
+
+        if k<0:
+            Imitation="below"
+        elif k == 0:
+            Imitation=""
+        else:
+            Imitation="above"
+
+        A=[]
+        B=0
+
+        for l in range (0,L-1):
+            if list(Lower_voice.items())[l][0]+k in Upper_voice:
+                if list(Lower_voice.items())[l+1][0]+k in Upper_voice:
+                    if DiagInterval(Lower_voice[list(Lower_voice.items())[l][0]][0],Upper_voice[list(Lower_voice.items())[l][0]+k][0]) == DiagInterval(Lower_voice[list(Lower_voice.items())[l+1][0]][0],Upper_voice[list(Lower_voice.items())[l+1][0]+k][0]):
+                        B=B+1
+                        if B==3:
+                            A.append("Measure {}, ".format(Lower_voice[list(Lower_voice.items())[l-2][0]][1])+Values[abs(k)]+" a "+DiagInterval(Lower_voice[list(Lower_voice.items())[l][0]][0],Upper_voice[list(Lower_voice.items())[l][0]+k][0])+" "+Imitation+";")
                         else:
-                            if A!=[]:
-                                A[-1]=A[-1]+" it ends measure {}".format(Piece_Lower_voice[list(Piece_Lower_voice.items())[l-1][0]][1])+"."
-                            else:
-                                pass
-                    else:
-                        pass
-                else:
-                    B=0
-                    if A!=[]:
-                        if A[-1].endswith("."):
                             pass
+                    elif DiagInterval(Lower_voice[list(Lower_voice.items())[l+1][0]][0],Upper_voice[list(Lower_voice.items())[l+1][0]+k][0])=='Rest':
+                        if list(Lower_voice.items())[l+2][0]+k in Upper_voice:
+                             if DiagInterval(Lower_voice[list(Lower_voice.items())[l][0]][0],Upper_voice[list(Lower_voice.items())[l][0]+k][0]) == DiagInterval(Lower_voice[list(Lower_voice.items())[l+2][0]][0],Upper_voice[list(Lower_voice.items())[l+2][0]+k][0]):
+                                 B=B+1
+                             else:
+                                 if A!=[]:
+                                     if A[-1].endswith("."):
+                                         pass
+                                     else:
+                                         A[-1]=A[-1]+" it ends measure {0} ({1} events)".format(Lower_voice[list(Lower_voice.items())[l][0]][1],B)+"."
+                                 else:
+                                     pass
+                                 B=0
                         else:
-                            A[-1]=A[-1]+" it ends measure {}".format(Piece_Lower_voice[list(Piece_Lower_voice.items())[l][0]][1])+"."
+                            pass
+                    elif DiagInterval(Lower_voice[list(Lower_voice.items())[l][0]][0],Upper_voice[list(Lower_voice.items())[l][0]+k][0])=='Rest':
+                        if list(Lower_voice.items())[l-1][0]+k in Upper_voice:
+                            if DiagInterval(Lower_voice[list(Lower_voice.items())[l-1][0]][0],Upper_voice[list(Lower_voice.items())[l-1][0]+k][0]) == DiagInterval(Lower_voice[list(Lower_voice.items())[l+1][0]][0],Upper_voice[list(Lower_voice.items())[l+1][0]+k][0]):
+                             B=B+1
+                            else:
+                                if A!=[]:
+                                    if A[-1].endswith("."):
+                                        pass
+                                    else:
+                                        A[-1]=A[-1]+" it ends measure {0} ({1} events)".format(Lower_voice[list(Lower_voice.items())[l][0]][1],B)+"."
+                                else:
+                                    pass
+                                B=0
+                        else:
+                            pass
                     else:
-                        pass
-        else:
-            if A!=[]:
-                if A[-1].endswith("."):
-                    pass
-                else:
-                    A[-1]=A[-1]+" it ends measure {}".format(Piece_Lower_voice[list(Piece_Lower_voice.items())[l][0]][1])+"."
+                        if A!=[]:
+                            if A[-1].endswith("."):
+                                pass
+                            else:
+                                A[-1]=A[-1]+" it ends measure {0} ({1} events)".format(Lower_voice[list(Lower_voice.items())[l][0]][1],B)+"."
+                        else:
+                            pass
+                        B=0
             else:
-                pass
-    if A!=[]:
-        if A[-1].endswith("."):
-            Interval_pattern.append(A)
+                if A!=[]:
+                    if A[-1].endswith("."):
+                        pass
+                    else:
+                        A[-1]=A[-1]+" it ends measure {0} ({1} events)".format(Lower_voice[list(Lower_voice.items())[l][0]][1],B)+"."
+                else:
+                    pass
+        if A!=[]:
+            if A[-1].endswith("."):
+                Interval_pattern.append(A)
+            else:
+                A[-1]=A[-1]+" it ends measure {0} ({1} events)".format(Lower_voice[list(Lower_voice.items())[l][0]][1],B)+"."
+                Interval_pattern.append(A)
         else:
-            A[-1]=A[-1]+" until the end of the piece."
-            Interval_pattern.append(A)
-    else:
-        pass
+            pass
+    return Interval_pattern
